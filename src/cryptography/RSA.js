1397be373d941f929b562a5317b7168c638dc353
AFz <afz.badie@gmail.com>
2024-08-30 00:17:53 +0330

    Fix RSA decrypt and encryption padding

--- src/cryptography/RSA.js
+++ src/cryptography/RSA.js
@@ -10,12 +10,12 @@ export const generateKeyPair = async () => {
 
 export const encrypt = async (pubKey, data) => {
     const publicKey = forge.pki.publicKeyFromPem(pubKey);
-    const encryptedMsg = publicKey.encrypt(data, 'RSA-OAEP');
+    const encryptedMsg = publicKey.encrypt(data);
     return forge.util.bytesToHex(encryptedMsg);
 }
 
 export const decrypt = async (privateKey, encryptedData) => {
     const privateKeyObj = forge.pki.privateKeyFromPem(privateKey);
     const encryptedBytes = forge.util.hexToBytes(encryptedData);
-    return privateKeyObj.decrypt(encryptedBytes, 'RSA-OAEP');
+    return privateKeyObj.decrypt(encryptedBytes);
 }
