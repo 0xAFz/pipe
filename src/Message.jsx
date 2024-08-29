2920f55e26458e0e9a5ad49ce42759f788e2c777
AFz <afz.badie@gmail.com>
2024-08-30 00:12:26 +0330

    Fix RSA decrypt and encryption

--- src/Message.jsx
+++ src/Message.jsx
@@ -34,7 +34,7 @@ function Message(props) {
 
     return (
         <div class="px-4 py-4 hover:bg-zinc-800 flex flex-col gap-2 border border-zinc-700 w-full rounded-md">
-            <p class="text-base">{decryptedMessage()}</p>
+            <p dir="rtl" class="text-base">{decryptedMessage()}</p>
             <span class="text-sm text-sky-300">{props.time}</span>
         </div>
     );
--- src/cryptography/RSA.js
+++ src/cryptography/RSA.js
@@ -1,18 +1,21 @@
-import forge from 'node-forge'
+import forge from 'node-forge';
 
 export const generateKeyPair = async () => {
-    const keys = await forge.pki.rsa.generateKeyPair({ bits: 4096, workers: 2 })
-    const pubKey = forge.pki.publicKeyToPem(keys.publicKey)
-    const privateKey = forge.pki.privateKeyToPem(keys.privateKey)
+    const keys = await forge.pki.rsa.generateKeyPair({ bits: 4096, workers: 2 });
+    const pubKey = forge.pki.publicKeyToPem(keys.publicKey);
+    const privateKey = forge.pki.privateKeyToPem(keys.privateKey);
 
-    return { privateKey, pubKey }
+    return { privateKey, pubKey };
 }
 
 export const encrypt = async (pubKey, data) => {
-    const encryptedMsg = forge.pki.publicKeyFromPem(pubKey).encrypt(data)
-    return forge.util.bytesToHex(encryptedMsg)
+    const publicKey = forge.pki.publicKeyFromPem(pubKey);
+    const encryptedMsg = publicKey.encrypt(data, 'RSA-OAEP');
+    return forge.util.bytesToHex(encryptedMsg);
 }
 
 export const decrypt = async (privateKey, encryptedData) => {
-    return forge.pki.privateKeyFromPem(privateKey).decrypt(forge.util.hexToBytes(encryptedData))
+    const privateKeyObj = forge.pki.privateKeyFromPem(privateKey);
+    const encryptedBytes = forge.util.hexToBytes(encryptedData);
+    return privateKeyObj.decrypt(encryptedBytes, 'RSA-OAEP');
 }
