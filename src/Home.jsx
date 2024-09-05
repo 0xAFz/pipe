e55548e38d3a48752312f5e51ec397a011ac8449
AFz <afz.badie@gmail.com>
2024-09-05 21:00:57 +0330

    Refactor get messages

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,35 +1,29 @@
-import { createSignal, onMount, Suspense, Show, For, onCleanup } from "solid-js";
+import { createSignal, createResource, Suspense, Show, For, onCleanup } from "solid-js";
 import axios from './axios';
 import Message from "./Message";
 
+const getMessages = async () => {
+  const response = await axios.get('/getMessages');
+  return response.data;
+};
+
 const formatUnixTime = (timestamp) => {
-  const date = new Date(timestamp * 1000); // Convert Unix time to milliseconds
-  const hours = date.getHours().toString().padStart(2, '0'); // Get hours and pad with zero if needed
-  const minutes = date.getMinutes().toString().padStart(2, '0'); // Get minutes and pad with zero if needed
+  const date = new Date(timestamp * 1000);
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
   return `${hours}:${minutes}`;
-}
+};
 
 function Home() {
+  const [messages, { mutate }] = createResource(getMessages);
   const [newMessageAnimation, setNewMessageAnimation] = createSignal(false);
-  const [messages, setMessages] = createSignal([]);
-
-  const getMessages = async () => {
-    try {
-      const response = await axios.get('/getMessages');
-      if (response.status === 200) {
-        setMessages(response.data);
-      }
-    } catch (error) {
-      console.error("Error loading messages:", error);
-    }
-  };
 
   const getUpdates = async () => {
     try {
       const response = await axios.get('/getUpdates?timeout=60');
       if (response.status === 200 && response.data.length > 0) {
         setNewMessageAnimation(true);
-        setMessages(prevMessages => [...response.data, ...prevMessages]);
+        mutate(prevMessages => [...response.data, ...prevMessages]);
       }
     } catch (error) {
       console.error("Error polling updates:", error);
@@ -38,9 +32,10 @@ function Home() {
     }
   };
 
-  onMount(() => {
-    getMessages();
-    getUpdates();
+  createResource(messages, () => {
+    if (messages()) {
+      getUpdates();
+    }
   });
 
   onCleanup(() => {
@@ -50,7 +45,7 @@ function Home() {
   return (
     <div class="flex flex-col items-center justify-center max-w-screen-md mx-auto gap-4">
       <Suspense fallback={<div dir="rtl">صبر کن پیاماتو بگیریم..</div>}>
-        <Show when={messages().length > 0} fallback={<div dir="rtl">فعلا هیچ پیامی نداری!</div>}>
+        <Show when={messages() && messages()?.length > 0} fallback={<div dir="rtl">فعلا هیچ پیامی نداری!</div>}>
           <For each={messages()}>
             {(message, index) => (
               <Message
